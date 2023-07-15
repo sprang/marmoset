d548662ff790f82e9705f807999f68caf197ca89
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-07-15 19:00:25 +0100

    Update GTK to 0.14

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,8 @@ extern crate gtk;
 extern crate num_traits;
 extern crate rand;
 extern crate serde;
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate serde_derive;
 extern crate serde_yaml;
 
 pub mod cell;
@@ -32,10 +33,10 @@ pub mod controller;
 pub mod game_state;
 pub mod rules;
 
+use gdk::prelude::*;
 use gdk::ModifierType;
-use gdk_pixbuf::{Pixbuf, PixbufLoader, PixbufLoaderExt};
+use gdk_pixbuf::{Pixbuf, PixbufLoader};
 use glib::{clone, Error};
-use gio::prelude::*;
 use gtk::prelude::*;
 use gtk::{AccelGroup, Application, ApplicationWindow, MenuItem};
 use std::cell::RefCell;
@@ -46,14 +47,17 @@ use crate::controller::Controller;
 use core::graphics::ColorScheme::{Classic, CMYK};
 
 /// A convenience type for passing data to menu building functions
-type MenuData<'a> = (&'a ApplicationWindow, &'a AccelGroup, &'a Rc<RefCell<Controller>>);
+type MenuData<'a> = (
+    &'a ApplicationWindow,
+    &'a AccelGroup,
+    &'a Rc<RefCell<Controller>>,
+);
 
 fn main() {
-    let app = Application::new(Some("org.nybble.marmoset"), Default::default())
-	.expect("Failed to initialize GTK application...");
+    let app = Application::new(Some("org.nybble.marmoset"), Default::default());
 
     app.connect_activate(|app| init(app));
-    app.run(&[]);
+    app.run();
 }
 
 fn init(app: &Application) {
@@ -95,12 +99,12 @@ fn build_window(app: &Application, controller: &Rc<RefCell<Controller>>) -> Appl
 
     // quit if the window is closed
     window.connect_delete_event(
-	clone!(@strong controller, @weak window => @default-return Inhibit(false), move |_, _| {
-	    // save the current window size in the config
-	    let config = &mut controller.borrow_mut().config;
-	    config.set_window_size(window.get_size());
-	    Inhibit(false)
-	})
+        clone!(@strong controller, @weak window => @default-return Inhibit(false), move |_, _| {
+            // save the current window size in the config
+            let config = &mut controller.borrow_mut().config;
+            config.set_window_size(window.size());
+            Inhibit(false)
+        }),
     );
 
     window
@@ -112,7 +116,7 @@ fn build_window(app: &Application, controller: &Rc<RefCell<Controller>>) -> Appl
 
 macro_rules! build_menu {
     ($menu:expr, [$( $e:expr ),*]) => {{
-	let menu = MenuItem::new_with_mnemonic($menu);
+	let menu = MenuItem::with_mnemonic($menu);
 	let submenu = gtk::Menu::new();
 	$( submenu.append(&$e); )*
 	menu.set_submenu(Some(&submenu));
@@ -120,13 +124,21 @@ macro_rules! build_menu {
     }}
 }
 
-fn make_menu_item(mnemonic: &str, accel_group: &AccelGroup,
-		  modifier: ModifierType, keys: &[char]) -> MenuItem
-{
-    let item = MenuItem::new_with_mnemonic(mnemonic);
+fn make_menu_item(
+    mnemonic: &str,
+    accel_group: &AccelGroup,
+    modifier: ModifierType,
+    keys: &[char],
+) -> MenuItem {
+    let item = MenuItem::with_mnemonic(mnemonic);
     for &key in keys.iter() {
-	item.add_accelerator("activate", accel_group, key as u32, modifier,
-			     gtk::AccelFlags::VISIBLE);
+        item.add_accelerator(
+            "activate",
+            accel_group,
+            key as u32,
+            modifier,
+            gtk::AccelFlags::VISIBLE,
+        );
     }
     item
 }
@@ -140,36 +152,37 @@ fn build_game_menu(menu_data: MenuData) -> MenuItem {
 
     // create menu items
     let new_game = make_menu_item("_New Game", accel_group, ModifierType::CONTROL_MASK, &['N']);
-    let restart = MenuItem::new_with_mnemonic("_Restart Game");
+    let restart = MenuItem::with_mnemonic("_Restart Game");
     let close = make_menu_item("_Close", accel_group, ModifierType::CONTROL_MASK, &['W']);
 
     new_game.connect_activate(
-	clone!(@strong controller => move |_| controller.borrow_mut().new_game())
+        clone!(@strong controller => move |_| controller.borrow_mut().new_game()),
     );
 
-    restart.connect_activate(
-	clone!(@strong controller => move |_| controller.borrow_mut().restart())
-    );
+    restart
+        .connect_activate(clone!(@strong controller => move |_| controller.borrow_mut().restart()));
 
-    close.connect_activate(
-	clone!(@weak window => move |_| window.close())
-    );
+    close.connect_activate(clone!(@weak window => move |_| window.close()));
 
     // disable restart menu by default
     restart.set_sensitive(false);
     // update restart status based on undo stack changes
     controller.borrow_mut().add_undo_observer(
-	clone!(@weak restart => move |controller| restart.set_sensitive(controller.can_undo()))
+        clone!(@weak restart => move |controller| restart.set_sensitive(controller.can_undo())),
     );
 
-    build_menu!("_Game",
-		[new_game,
-		 restart,
-		 gtk::SeparatorMenuItem::new(),
-		 build_variant_submenu(menu_data),
-		 build_deck_submenu(menu_data),
-		 gtk::SeparatorMenuItem::new(),
-		 close])
+    build_menu!(
+        "_Game",
+        [
+            new_game,
+            restart,
+            gtk::SeparatorMenuItem::new(),
+            build_variant_submenu(menu_data),
+            build_deck_submenu(menu_data),
+            gtk::SeparatorMenuItem::new(),
+            close
+        ]
+    )
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -180,29 +193,25 @@ fn build_variant_submenu(menu_data: MenuData) -> MenuItem {
     let (window, _accel_group, controller) = menu_data;
 
     // create menu items
-    let set_variant = gtk::RadioMenuItem::new_with_mnemonic("_Set");
-    let superset_variant = gtk::RadioMenuItem::new_with_mnemonic("S_uperSet");
+    let set_variant = gtk::RadioMenuItem::with_mnemonic("_Set");
+    let superset_variant = gtk::RadioMenuItem::with_mnemonic("S_uperSet");
     superset_variant.join_group(Some(&set_variant));
 
     // reflect config settings
     match controller.borrow().config.variant {
-	Variant::Set => set_variant.set_active(true),
-	Variant::SuperSet => superset_variant.set_active(true)
+        Variant::Set => set_variant.set_active(true),
+        Variant::SuperSet => superset_variant.set_active(true),
     }
 
-    set_variant.connect_toggled(
-	clone!(@strong controller, @weak window => move |_| {
-	    controller.borrow_mut().set_variant(Variant::Set);
-	    window.set_title("Set");
-	})
-    );
+    set_variant.connect_toggled(clone!(@strong controller, @weak window => move |_| {
+        controller.borrow_mut().set_variant(Variant::Set);
+        window.set_title("Set");
+    }));
 
-    superset_variant.connect_toggled(
-	clone!(@strong controller, @weak window => move |_| {
-	    controller.borrow_mut().set_variant(Variant::SuperSet);
-	    window.set_title("SuperSet");
-	})
-    );
+    superset_variant.connect_toggled(clone!(@strong controller, @weak window => move |_| {
+        controller.borrow_mut().set_variant(Variant::SuperSet);
+        window.set_title("SuperSet");
+    }));
 
     build_menu!("_Variant", [set_variant, superset_variant])
 }
@@ -215,25 +224,21 @@ fn build_deck_submenu(menu_data: MenuData) -> MenuItem {
     let (_window, _accel_group, controller) = menu_data;
 
     // create menu items
-    let beginner_deck = gtk::RadioMenuItem::new_with_mnemonic("_Beginner");
-    let full_deck = gtk::RadioMenuItem::new_with_mnemonic("_Full");
+    let beginner_deck = gtk::RadioMenuItem::with_mnemonic("_Beginner");
+    let full_deck = gtk::RadioMenuItem::with_mnemonic("_Full");
     full_deck.join_group(Some(&beginner_deck));
 
     // reflect config settings
     match controller.borrow().config.deck {
-	Deck::Simplified => beginner_deck.set_active(true),
-	Deck::Full => full_deck.set_active(true)
+        Deck::Simplified => beginner_deck.set_active(true),
+        Deck::Full => full_deck.set_active(true),
     }
 
-    beginner_deck.connect_toggled(
-	clone!(@strong controller => move |_|
-	       controller.borrow_mut().set_deck(Deck::Simplified))
-    );
+    beginner_deck.connect_toggled(clone!(@strong controller => move |_|
+	       controller.borrow_mut().set_deck(Deck::Simplified)));
 
-    full_deck.connect_toggled(
-	clone!(@strong controller => move |_|
-	       controller.borrow_mut().set_deck(Deck::Full))
-    );
+    full_deck.connect_toggled(clone!(@strong controller => move |_|
+	       controller.borrow_mut().set_deck(Deck::Full)));
 
     build_menu!("_Deck", [beginner_deck, full_deck])
 }
@@ -243,37 +248,33 @@ fn build_deck_submenu(menu_data: MenuData) -> MenuItem {
 ////////////////////////////////////////////////////////////////////////////////
 
 fn connect_undo_redo(controller: &Rc<RefCell<Controller>>, undo: &MenuItem, redo: &MenuItem) {
-    undo.connect_activate(
-	clone!(@strong controller => move |_| controller.borrow_mut().undo())
-    );
+    undo.connect_activate(clone!(@strong controller => move |_| controller.borrow_mut().undo()));
 
-    redo.connect_activate(
-	clone!(@strong controller => move |_| controller.borrow_mut().redo())
-    );
+    redo.connect_activate(clone!(@strong controller => move |_| controller.borrow_mut().redo()));
 
     // undo and redo are disabled by default
     undo.set_sensitive(false);
     redo.set_sensitive(false);
 
     // update undo/redo status based on undo stack changes
-    controller.borrow_mut().add_undo_observer(
-	clone!(@weak undo, @weak redo => move |controller| {
-	    undo.set_sensitive(controller.can_undo());
-	    redo.set_sensitive(controller.can_redo());
+    controller
+        .borrow_mut()
+        .add_undo_observer(clone!(@weak undo, @weak redo => move |controller| {
+            undo.set_sensitive(controller.can_undo());
+            redo.set_sensitive(controller.can_redo());
 
-	    if let Some(action) = controller.undo_action_name() {
-		undo.set_label(&format!("_Undo {}", action));
-	    } else {
-		undo.set_label("_Undo");
-	    }
+            if let Some(action) = controller.undo_action_name() {
+                undo.set_label(&format!("_Undo {}", action));
+            } else {
+                undo.set_label("_Undo");
+            }
 
-	    if let Some(action) = controller.redo_action_name() {
-		redo.set_label(&format!("_Redo {}", action));
-	    } else {
-		redo.set_label("_Redo");
-	    }
-	})
-    );
+            if let Some(action) = controller.redo_action_name() {
+                redo.set_label(&format!("_Redo {}", action));
+            } else {
+                redo.set_label("_Redo");
+            }
+        }));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -291,8 +292,8 @@ fn build_control_menu(menu_data: MenuData) -> MenuItem {
     let redo = make_menu_item("_Redo", accel_group, ctrl_shift, &['Z']);
     let hint = make_menu_item("_Hint", accel_group, no_modifier, &['?', '/']);
     let deal_more = make_menu_item("_Deal More Cards", accel_group, no_modifier, &['+', '=']);
-    let tidy_layout = gtk::CheckMenuItem::new_with_mnemonic("_Tidy Layout");
-    let classic_colors = gtk::CheckMenuItem::new_with_mnemonic("_Classic Colors");
+    let tidy_layout = gtk::CheckMenuItem::with_mnemonic("_Tidy Layout");
+    let classic_colors = gtk::CheckMenuItem::with_mnemonic("_Classic Colors");
 
     // reflect config settings
     tidy_layout.set_active(config.tidy_layout);
@@ -301,41 +302,37 @@ fn build_control_menu(menu_data: MenuData) -> MenuItem {
     // undo and redo require a bit more setup than other menu items
     connect_undo_redo(controller, &undo, &redo);
 
-    hint.connect_activate(
-	clone!(@strong controller, @weak window => move |_| {
-	    let message = controller.borrow_mut().show_hint();
-	    show_message_dialog(message, &window);
-	})
-    );
+    hint.connect_activate(clone!(@strong controller, @weak window => move |_| {
+        let message = controller.borrow_mut().show_hint();
+        show_message_dialog(message, &window);
+    }));
 
-    deal_more.connect_activate(
-	clone!(@strong controller, @weak window => move |_| {
-	    let message = controller.borrow_mut().deal_more_cards();
-	    show_message_dialog(message, &window);
-	})
-    );
+    deal_more.connect_activate(clone!(@strong controller, @weak window => move |_| {
+        let message = controller.borrow_mut().deal_more_cards();
+        show_message_dialog(message, &window);
+    }));
 
-    tidy_layout.connect_toggled(
-	clone!(@strong controller => move |w|
-	       controller.borrow_mut().set_tidy_layout(w.get_active()))
-    );
+    tidy_layout.connect_toggled(clone!(@strong controller => move |w|
+	       controller.borrow_mut().set_tidy_layout(w.is_active())));
 
-    classic_colors.connect_toggled(
-	clone!(@strong controller => move |w|  {
-	    let scheme = if w.get_active() { Classic } else { CMYK };
-	    controller.borrow_mut().set_color_scheme(scheme);
-	})
-    );
+    classic_colors.connect_toggled(clone!(@strong controller => move |w|  {
+        let scheme = if w.is_active() { Classic } else { CMYK };
+        controller.borrow_mut().set_color_scheme(scheme);
+    }));
 
-    build_menu!("_Control",
-		[undo,
-		 redo,
-		 gtk::SeparatorMenuItem::new(),
-		 hint,
-		 deal_more,
-		 gtk::SeparatorMenuItem::new(),
-		 tidy_layout,
-		 classic_colors])
+    build_menu!(
+        "_Control",
+        [
+            undo,
+            redo,
+            gtk::SeparatorMenuItem::new(),
+            hint,
+            deal_more,
+            gtk::SeparatorMenuItem::new(),
+            tidy_layout,
+            classic_colors
+        ]
+    )
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -349,7 +346,7 @@ static COMMENT: &str = include_str!("../resources/description.txt");
 fn logo_loader() -> Result<PixbufLoader, Error> {
     let data = include_bytes!("../resources/logo.png");
 
-    let loader = PixbufLoader::new_with_type("png")?;
+    let loader = PixbufLoader::with_type("png")?;
     loader.write(data)?;
     loader.close()?;
 
@@ -358,30 +355,30 @@ fn logo_loader() -> Result<PixbufLoader, Error> {
 
 fn logo() -> Option<Pixbuf> {
     match logo_loader() {
-	Ok(loader) => loader.get_pixbuf(),
-	Err(_) => None,
+        Ok(loader) => loader.pixbuf(),
+        Err(_) => None,
     }
 }
 
 fn build_help_menu(window: &ApplicationWindow) -> MenuItem {
-    let about = MenuItem::new_with_mnemonic("_About");
-    about.connect_activate(
-	clone!(@weak window => move |_| {
-	    let a = gtk::AboutDialog::new();
-	    a.set_program_name("Marmoset");
-	    a.set_logo(logo().as_ref());
-	    a.set_comments(Some(COMMENT));
-	    a.set_copyright(Some("Copyright © 2017-2020 Steve Sprang"));
-	    a.set_license_type(gtk::License::Gpl30);
-	    a.set_license(Some(LICENSE));
-	    a.set_website(Some("https://github.com/sprang/marmoset"));
-	    a.set_website_label(Some("Marmoset Website"));
-	    a.set_version(Some(VERSION));
-	    a.set_transient_for(Some(&window));
-	    a.run();
-	    a.destroy();
-	})
-    );
+    let about = MenuItem::with_mnemonic("_About");
+    about.connect_activate(clone!(@weak window => move |_| {
+        let a = gtk::AboutDialog::new();
+        a.set_program_name("Marmoset");
+        a.set_logo(logo().as_ref());
+        a.set_comments(Some(COMMENT));
+        a.set_copyright(Some("Copyright © 2017-2020 Steve Sprang"));
+        a.set_license_type(gtk::License::Gpl30);
+        a.set_license(Some(LICENSE));
+        a.set_website(Some("https://github.com/sprang/marmoset"));
+        a.set_website_label(Some("Marmoset Website"));
+        a.set_version(Some(VERSION));
+        a.set_transient_for(Some(&window));
+        a.run();
+        unsafe {
+        a.destroy();
+        }
+    }));
 
     build_menu!("_Help", [about])
 }
@@ -392,13 +389,17 @@ fn build_help_menu(window: &ApplicationWindow) -> MenuItem {
 
 fn show_message_dialog(message: Option<String>, window: &ApplicationWindow) {
     if let Some(string) = message {
-	let md = gtk::MessageDialog::new(Some(window),
-					 gtk::DialogFlags::empty(),
-					 gtk::MessageType::Info,
-					 gtk::ButtonsType::Ok,
-					 &string);
-	md.set_markup(&format!("<big>{}</big>", string));
-	md.run();
-	md.destroy();
+        let md = gtk::MessageDialog::new(
+            Some(window),
+            gtk::DialogFlags::empty(),
+            gtk::MessageType::Info,
+            gtk::ButtonsType::Ok,
+            &string,
+        );
+        md.set_markup(&format!("<big>{}</big>", string));
+        md.run();
+        unsafe {
+            md.destroy();
+        }
     }
 }
